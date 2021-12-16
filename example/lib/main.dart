c123f51780f782d7a096c62a882f44ddd4f35692
jasmeen7202 <jasmeen7202@gmail.com>
2021-12-16 20:50:34 +0530

    image widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,72 +1,57 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:example_app/example_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key: key);
-  
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      
-    )
-  }
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await ExampleApp.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: Text(
+            'Hello!!',
+            style: TextStyle(color: Colors.red, fontSize: 30),
+          ),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello!!'),
+            Text('Hello!!'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.home,
+                  color: Colors.green,
+                  size: 90,
+                ),
+                Text('Girlscript'),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              height: 150,
+              width: 150,
+              color: Colors.greenAccent,
+              child: Icon(
+                Icons.home,
+                color: Colors.red,
+                size: 150,
+              ),
+            ),
+            Image(
+                height: 50,
+                width: 50,
+                image: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/57723319?v=4'))
+          ],
+        )),
       ),
     );
   }
